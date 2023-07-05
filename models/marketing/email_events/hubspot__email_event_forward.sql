d9d776d00ecb178ce49f87a2d190a2504af144d7
jacobrosen503 <106923974+jacobrosen503@users.noreply.github.com>
2023-07-05 01:02:41 -0500

    Update hubspot__email_event_forward.sql

--- models/marketing/email_events/hubspot__email_event_forward.sql
+++ models/marketing/email_events/hubspot__email_event_forward.sql
@@ -1,5 +1,3 @@
 {{ config(enabled=fivetran_utils.enabled_vars(['hubspot_marketing_enabled','hubspot_email_event_enabled','hubspot_email_event_forward_enabled'])) }}
 
-{{ email_events_joined(var('email_event_forward')) }}
-
-DROP hubspot.email_event_forward IF EXISTS
+{{ email_events_joined(var('email_event_clicks')) }}
